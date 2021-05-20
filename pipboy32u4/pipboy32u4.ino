434a8917dacf45661a1d662b94ba670db80cfafd
poevoid <65972401+poevoid@users.noreply.github.com>
2021-05-20 14:18:24 -0700

    Update pipboy32u4.ino
    badusb support, tinyfont, and joystick

--- pipboy32u4/pipboy32u4.ino
+++ pipboy32u4/pipboy32u4.ino
@@ -1,8 +1,15 @@
 //ardu-pip-boy
-
+#include <Keyboard.h>
 #include <Arduboy2.h>
-
+#include <Tinyfont.h>
+#include <Joystick.h>
 Arduboy2 pipboy;
+
+#define JOYSTICK_REPORT_ID 0x03
+Tinyfont tinyfont= Tinyfont(pipboy.sBuffer, Arduboy2::width(), Arduboy2::height());
+// Create the Joystick with 4 buttons
+
+
 #include "func.h"
 
 void setup() {
@@ -10,7 +17,7 @@ void setup() {
   pipboy.begin();
   pipboy.clear();
   pipboy.setFrameRate(60); //no need to waste power, right?
-  
+  Keyboard.begin();
 }
 
 void loop() {
