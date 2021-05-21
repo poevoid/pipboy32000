3bca6eeebf128555d5fc3588d4c89545e400b120
poevoid <65972401+poevoid@users.noreply.github.com>
2021-05-20 18:38:22 -0700

    Update vars.h
    gnome commands

--- pipboy32u4/vars.h
+++ pipboy32u4/vars.h
@@ -3,16 +3,17 @@
 #define USB 1
 #define MAP 2
 #define RAD 3
+#define JOYSTICK_REPORT_ID 0x03
 
 uint8_t gamestate = 0;
 uint8_t currentframe = 0;
 uint8_t firstframe = 0;
 uint8_t framecount = 7; //1 less than actual number of frames, first frame = 0
 uint8_t startcounter = 0;
-
 uint8_t mainMenu = INV;
 uint8_t USBselect = 0;
 uint8_t INVselect = 0;
+uint8_t RADselect = 0;
 uint8_t INVcursorx;
 uint8_t INVcursory;
 
@@ -24,6 +25,7 @@ const int buttonHeldTime = 300, buttonReleaseTime = 1000;
 /*/////////////////////////////////////////////////////////////////////////////////////////////////*/
 
 extern void CommandAtRunBarMSWIN(char *SomeCommand); //Opens the run bar and executes the command.
+extern void CommandAtRunBarGnome(char *SomeCommand);
 
 /********************************************************************
  * Opens the run bar and executes the command. 
@@ -31,6 +33,16 @@ extern void CommandAtRunBarMSWIN(char *SomeCommand); //Opens the run bar and exe
 void CommandAtRunBarMSWIN(char *SomeCommand){
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
+  delay(100);
+  Keyboard.releaseAll();
+  delay(1500);
+  Keyboard.print(SomeCommand);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+}
+void CommandAtRunBarGnome(char *SomeCommand){
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F2);
   delay(100);
   Keyboard.releaseAll();
   delay(1500);
